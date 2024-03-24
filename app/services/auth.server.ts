26ddd0de6cd62c94710dea0ee43b849b819d0d60
Daniel Steman <daniel-steman@live.nl>
2024-03-24 19:21:07 +0100

    github auth

---
+++ app/services/auth.server.ts
@@ -0,0 +1,32 @@
+// app/services/auth.server.ts
+import { Authenticator } from "remix-auth";
+import { sessionStorage } from "./session.server";
+import { GitHubStrategy } from "remix-auth-github";
+
+import "dotenv/config";
+
+interface User {
+  token: string;
+}
+
+const gitHubStrategy = new GitHubStrategy(
+  {
+    clientID: process.env.GITHUB_OAUTH_CLIENT_ID!,
+    clientSecret: process.env.GITHUB_OAUTH_CLIENT_SECRET!,
+    callbackURL: "http://localhost:3000/auth/github/callback",
+  },
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async ({ accessToken, extraParams, profile }) => {
+    // Get the user data from your DB or API using the tokens and profile
+    // return User.findOrCreate({ email: profile.emails[0].value });
+    const user: User = { token: "abc" };
+    return user;
+  }
+);
+
+// Create an instance of the authenticator, pass a generic with what
+// strategies will return and will store in the session
+const authenticator = new Authenticator<User>(sessionStorage);
+authenticator.use(gitHubStrategy);
+
+export default authenticator;
